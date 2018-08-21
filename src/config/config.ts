e75987df59795037b60d666c2db000a4c86851c3
MariannaSorrentino <40571749+MariannaSorrentino@users.noreply.github.com>
2018-08-21 20:40:00 +0200

    eliminata forzatura ambiente

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ import * as data from './config.json';
 
 
 //per provare le API su app server prod - poi rimuovere
-environment.production = true;
+//environment.production = true;
 const dataAmbiente = environment.production? data['prod'] : data['svil'];
 
 const config = new Object();
